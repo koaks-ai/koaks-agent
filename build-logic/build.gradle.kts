28a9d8129cb5b1662d21db4fb092f275360421b7
gmjneko <gemingjia0201@163.com>
2026-08-13 22:42:11 +0800

    chore: update Kotlin dependencies to version 2.4.10

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.20")
-    implementation("org.jetbrains.kotlin:kotlin-serialization:2.2.20")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:2.4.10")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:7.2.1")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:9.2.2")
 }
